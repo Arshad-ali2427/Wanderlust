cc5ce6fd5f5dcc0dd192cf5e3a2ef6b11c49e6e8
Arshad ali <arshadali242766@gmail.com>
2025-04-06 14:18:23 +0530

    changee

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
+//jjxj
 
 // MongoDB Connection
 const dbUrl = process.env.ATLASDB_URL; 
